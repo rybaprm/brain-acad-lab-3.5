b68676fb6c473c7d44c67ef2ca6895b7f0346450
rybaprm <59397258+rybaprm@users.noreply.github.com>
2020-08-18 13:30:55 +0300

    even or odd number

---
+++ src/ua/zp/brainacad/Main.java
@@ -0,0 +1,20 @@
+package ua.zp.brainacad;
+
+public class Main {
+    /**
+     * Class Main output to consol different types of variables
+     */
+    public static void main(String[] args) {
+        System.out.println(1 % 2 == 0 ? "It's even number" : "It's odd number");
+        System.out.println(2 % 2 == 0 ? "It's even number" : "It's odd number");
+        System.out.println(3 % 2 == 0 ? "It's even number" : "It's odd number");
+        System.out.println(4 % 2 == 0 ? "It's even number" : "It's odd number");
+        System.out.println(5 % 2 == 0 ? "It's even number" : "It's odd number");
+        System.out.println(6 % 2 == 0 ? "It's even number" : "It's odd number");
+        System.out.println(7 % 2 == 0 ? "It's even number" : "It's odd number");
+        System.out.println(8 % 2 == 0 ? "It's even number" : "It's odd number");
+        System.out.println(9 % 2 == 0 ? "It's even number" : "It's odd number");
+        System.out.println(10 % 2 == 0 ? "It's even number" : "It's odd number");
+
+    }
+}
